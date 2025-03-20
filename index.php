584dcd50b0126739f46869f1b7d5463caf48f521
Matheus <matheus8142@hotmail.com>
2025-03-20 17:42:28 -0300

    Iniciando projeto da calculadora
    Foi criado a tela da calculadora junto com 9 botões

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pages/assets/style.css">
+    <title>Calculadora Master</title>
+</head>
+<body>
+    <div class="container">
+        <div id="container-calc">
+            <div id="container-tela">
+                <div id="tela">
+                    <div id="text-tela">alo diabo</div>
+                </div>
+            </div>
+        </div>
+        <div class="container-teclas">
+            <div id="teclas">
+                <div class="container-num">
+                    <div class="num">1</div>
+                    <div class="num">2</div>
+                    <div class="num">3</div>
+                </div>
+                <div class="container-num">
+                    <div class="num">4</div>
+                    <div class="num">5</div>
+                    <div class="num">6</div>
+                </div>
+                <div class="container-num">
+                    <div class="num">7</div>
+                    <div class="num">8</div>
+                    <div class="num">9</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
