c1f698e03f2e3cae12f600327c34fde2951caa06
Matheus <matheus8142@hotmail.com>
2025-04-10 16:56:07 -0300

    Finalizando calculadora master nessa atualização foi adicionado:
    *função de limpar a tela
    *bloco de codigo para realizar o calculo das operações
    *exibição das operações em tempo real na tela

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
-$(document).ready(function(){
-    var valor1 = 1
-    var valor2 = 1
-    var resultado = 0
+$(document).ready(function () {
+    var valor1 = '';
+    var valor2 = '';
+    var operador = '';
+    var operacao = '';
+    var resultado = '';
 
-    $('#num1').click(function() {
-        var valor1 = this.value
+    //Função usada para pegar o operador da operação
+    $('.operador').click(function () {
+        operador = $(this).val();
+        $('#text-tela').text(valor1 + operador);
     });
 
-    $('#num2').click(function(){
-        var valor2 = 2
+    //Função usada para pegar os valores com uma condição que decide qual é o primeiro
+    $('.num').click(function () {
+        if (operador === '') {
+            valor1 += $(this).val();
+            $('#text-tela').text(valor1);
+        } else {
+            valor2 += $(this).val();
+            $('#text-tela').text(valor2);
+        }
+        operacao = valor1 + operador + valor2
+        $('#text-tela').text(operacao);
     });
 
-    $("#adição").click(function(){
-        var resultado = valor1 + valor2
+    //Função que executa as operações matemáticas
+    $('#result').click(function () {
+        let num1 = parseFloat(valor1);
+        let num2 = parseFloat(valor2);
+
+
+        switch (operador) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '/':
+                resultado = num2 !== 0 ? num1 / num2 : 'Erro';
+                break;
+
+        }
+        $('#text-tela').text(resultado);
     });
 
-    $('#result').click(function(){
-        $('#text-tela').text('num1');
+    //Função para limpar a tela
+    $('#limpartela').click(function () {
+        num1 = '';
+        num2 = '';
+        valor1 = '';
+        valor2 = '';
+        operador = '';
+        resultado = '';
+
+        $('#text-tela').text('');
     });
 });
